f9401e79349d8dc88a8b2f9cd58d9ce2e7149e2f
Julian Nunes <julian.jgn@gmail.com>
2022-01-26 21:09:00 -0300

    Including add/remove btns

--- src/components/SortNumbers/index.jsx
+++ src/components/SortNumbers/index.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { useNavigate } from "react-router-dom";
+
+import swal from 'sweetalert';
 
 import Header from '../Header';
 import MainMenu from '../MainMenu';
 import { BigBtn, BigInput } from '../UI'
+
+import './style.css'
 
 function SortNumbers() {
     const [numbers, setNumbers] = useState(0);
@@ -17,7 +20,15 @@ function SortNumbers() {
     const sortedNumbers = [];
 
     function doSortNumbers() {
-        if(!numbers) return
+        if(!numbers) {
+            swal('Por favor, escolha ao menos um número')
+            return
+        }
+        
+        if(numbers < 0) {
+            swal(`Por favor, escolha um número entre 0 e ${limitNumber}`)
+            return
+        }
 
         let _sortedNumbers = [...sortedNumbers];
 
@@ -34,12 +45,26 @@ function SortNumbers() {
         navigate("../numbers", { replace: true });       
     }
 
+    function addNumber() {
+        if(numbers < limitNumber)
+            setNumbers(numbers + 1)
+    }
+
+    function removeNumber() {
+        if(numbers > 0)
+            setNumbers(numbers - 1)
+    }
+
     return (
         <>
             <Header />
             <section>
                 <h1>Quero sortear...</h1>
-                <BigInput onChange={(e) => setNumbers(e.target.value)} type="number" id="random-input" name="random-input" autoComplete="off" autoFocus maxLength={limitNumber.toString().length} max={limitNumber} />
+                <div className='input-container'>
+                    <button onClick={() => removeNumber()} className='btn-clean btn-numbers'>-</button>
+                    <BigInput onChange={(e) => setNumbers(+e.target.value)} value={numbers} type="number" id="random-input" name="random-input" autoComplete="off" autoFocus maxLength={limitNumber.toString().length} min={0} max={limitNumber} />
+                    <button onClick={() => addNumber()} className='btn-clean btn-numbers'>+</button>
+                </div>
                 <p>
                     <BigBtn className='btn-clean' onClick={() => doSortNumbers()}>sortear</BigBtn>
                     <button className='btn-clean'><i className="fas fa-edit"></i></button>
